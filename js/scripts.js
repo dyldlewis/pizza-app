d0d95c7ae5fe9f1b3a9a88e1b01848c4606e53da
Dylan Lewis <dyldlewis@gmail.com>
2017-06-16 13:26:20 -0300

    update cost method to account for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,27 @@ function Order(orderName, size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.cost = 0;
-}
+};
 
 
-Order.prototype.cost = function() {
+Order.prototype.sizeCost = function() {
   if (this.size === "Small") {
-    this.cost = 8;
+      this.cost = 8 + (2.50 * this.toppings.length);
   } else if (this.size === "Medium") {
-    this.cost = 10;
+    this.cost = 10 + (2.50 * this.toppings.length);
   } else if (this.size === "Large") {
-    this.cost = 12;
+    this.cost = 12 + (2.50 * this.toppings.length)
   } else if (this.size === "Extra Large") {
-    this.cost = 14;
+    this.cost = 14 + (2.50 * this.toppings.length)
   }
   return this.cost;
 };
 
-
+// Order.prototype.totalCost = function(cost) {
+//   if (this.toppings.length === 1) {
+//     this.cost = cost + 2.50;
+//   }
+// };
 
 
 
@@ -45,8 +49,9 @@ $(document).ready(function() {
     $("input:checkbox[name=toppings]:checked").each(function() {
       var topping = $(this).val();
       toppings.push(topping);
-
     });
+    var cost = newOrder.sizeCost()
+    $("#reciept").text(cost)
     console.log(orderName);
     console.log(size);
     console.log(toppings);
